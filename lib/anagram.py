73c2d75e46c76074ce8349642b5ff06309d10ac4
Melanie Wilson <melaniemwilson1@gmail.com>
2024-04-15 21:55:53 -0400

    completed lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        matched_word = [letter for letter in self.word]
+        anagrams = []
+        for word in word_list:
+            if (sorted(list(word)) == sorted(matched_word)):
+                anagrams.append(word)
+        return anagrams
